2720b8391fd03a944566b2584f69216d0fbf4c33
Mahendra Dinata <remdinata@gmail.com>
2024-05-16 18:12:12 +0700

    init project

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <div className="p-20 space-y-6">
+      <h1 className="text-5xl text-sky-500 font-bold text-opacity-80">
+        Expert System
+      </h1>
+      <h3 className="text-3xl font-bold">
+        Sistem Penilaian Kelayakan Investasi <br /> Pertambangan
+      </h3>
+      <p className="text-lg">
+        Optimalkan investasi pertambangan anda dengan sistem <br /> pakar kami.
+        Evaluasi kelayakan dengan cepat dan akurat. <br /> Mulai tingkatkan
+        keuntungan anda hari ini.
+      </p>
+      <button className="btn-primary">Get Started</button>
+    </div>
+  );
+}
